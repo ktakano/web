5cc6f105b2740fa36f23c15ed5b0cb4be05c82e5
Kosuke Takano <takano@ic.kanagawa-it.ac.jp>
2022-05-04 12:17:24 +0900

    Inital commit

---
+++ perl/pl001.pl
@@ -0,0 +1,9 @@
+#!"C:\xampp\perl\bin\perl.exe"
+
+print "Content-Type: text/html;charset=UTF-8\n\n";
+print '<html>';
+print '<head>';
+print '<meta charset="UTF-8">';
+print '</head>';
+print "<body><h1>Web技術入門</h1>";
+print "みなさん、こんにちは!</body></html>";
