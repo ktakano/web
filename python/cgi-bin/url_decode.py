0f112539a05c6139d75cfc3106252b75372c6a7c
Kosuke Takano <takano@ic.kanagawa-it.ac.jp>
2022-05-04 12:00:30 +0900

    Initial commit

---
+++ python/cgi-bin/url_decode.py
@@ -0,0 +1,13 @@
+import re
+
+value = "%E9%B7%B9%E9%87%8E"; # "鷹野"
+
+# 正規表現
+regexp = re.compile('%([a-fA-F0-9][a-fA-F0-9])')
+
+# group(1)は、%〇〇の〇〇を取り出す
+value = re.sub(regexp,lambda x:x.group(1), value) # value = value.replace('%', ' ')と同じ処理
+
+val = bytes.fromhex(value) # 16進数文字列を、バイト文字列に変換
+
+print ("URLデコード (E9 B7 B9 E9 87 8E): " + val.decode('utf8'))
