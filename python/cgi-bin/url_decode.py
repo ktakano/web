f7dbc9ee19745563f3efc7e3b25237fc1174d335
Kosuke Takano <takano@ic.kanagawa-it.ac.jp>
2022-05-04 12:03:34 +0900

    Modified valuable name: val -> value

--- python/cgi-bin/url_decode.py
+++ python/cgi-bin/url_decode.py
@@ -8,6 +8,6 @@ regexp = re.compile('%([a-fA-F0-9][a-fA-F0-9])')
 # group(1)は、%〇〇の〇〇を取り出す
 value = re.sub(regexp,lambda x:x.group(1), value) # value = value.replace('%', ' ')と同じ処理
 
-val = bytes.fromhex(value) # 16進数文字列を、バイト文字列に変換
+value = bytes.fromhex(value) # 16進数文字列を、バイト文字列に変換
 
-print ("URLデコード (E9 B7 B9 E9 87 8E): " + val.decode('utf8'))
+print ("URLデコード (E9 B7 B9 E9 87 8E): " + value.decode('utf8')) # バイト文字列を文字列(UTF8)に変換して表示
