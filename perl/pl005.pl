9fed9a7c6916ec586ec698296d5c75a2bef109a7
ktakano <takano@ic.kanagawa-it.ac.jp>
2022-05-09 15:53:39 +0900

    Initial commit

---
+++ perl/pl005.pl
@@ -0,0 +1,37 @@
+#!"C:\xampp\perl\bin\perl.exe"
+
+readFormData();
+print "Content-type: text/html\n\n";
+print '<html>';
+print '<head>';
+print '<meta http-equiv="content-type" content="text/html; charset=UTF-8">';
+print '</head><body>';
+print $FORM{"my_time"} . "時間後の下荻野の天気:<br>";
+$mytime = $FORM{"my_time"};
+$tenki = "晴れ";
+if ($mytime>=0 && $mytime<2) {$tenki = "曇り"; $color="gray";}
+elsif ($mytime>=2 && $mytime<4) {$tenki = "晴れ"; $color="red";}
+elsif ($mytime>=4 && $mytime<6) {$tenki = "雨"; $color="blue";}
+print '<div style="color: '. $color . ';">' . $tenki . "</div>";
+print "</body></html>";
+
+# フォームのデータから，パラメータ値を取得する関数
+sub readFormData
+{
+	my ($buffer, $pair);
+	if ($ENV{'REQUEST_METHOD'} eq 'POST') {
+		read (STDIN, $buffer, $ENV{ 'CONTENT_LENGTH' });
+	} else {
+		$buffer = $ENV { 'QUERY_STRING' };
+	}
+
+    foreach $pair (split(/&/, $buffer)) {
+		my ($name, $value) = split (/=/, $pair);
+		# デコード
+		$value =~ tr/+/ /;         
+		$value =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C", hex($1))/eg;        
+
+		$FORM {$name} = $value;
+    }
+
+}
